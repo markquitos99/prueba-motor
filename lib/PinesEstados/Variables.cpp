f0e4a570a2754d448ae4476dc9f7061b2e1518d2
Marcos Lizzano <marcos.lizzano@gmail.com>
2024-07-30 13:56:30 -0300

    modified:   lib/PinesEstados/Variables.cpp

--- lib/PinesEstados/Variables.cpp
+++ lib/PinesEstados/Variables.cpp
@@ -1,18 +1,18 @@
 #include "Variables.h"
 
-int STEP_PIN_X =23;
-int DIR_PIN_X = 25;
-int ENABLE_PIN_X = 22;
-int STEP_PIN_Y = 27;
-int DIR_PIN_Y = 32;
-int ENABLE_PIN_Y = 26;
-int STEP_PIN_E = 5;
+int STEP_PIN_X =22;
+int DIR_PIN_X = 23;
+int ENABLE_PIN_X = 21;
+int STEP_PIN_Y = 33;
+int DIR_PIN_Y = 25;
+int ENABLE_PIN_Y = 32;
+int STEP_PIN_E = 2;
 int DIR_PIN_E = 4;
-int ENABLE_PIN_E = 33;
+int ENABLE_PIN_E = 15;
  
 // Pines para los finales de carrera
-int FDCARRERA_X = 34;
-int FDCARRERA_Y = 35;
+int FDCARRERA_X = 19;
+int FDCARRERA_Y = 18;
 
 // Variables para almacenar los comandos de G-code
 int destinoProximoX = 0;
