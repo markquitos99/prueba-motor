2d7ce6cfd57166448df3606191a1654467a6be6c
Marcos Lizzano <marcos.lizzano@gmail.com>
2024-08-05 18:17:42 -0300

    probandoMandarGcode

--- Proyecto/Proyecto/views.py
+++ Proyecto/Proyecto/views.py
@@ -7,12 +7,20 @@ import serial
 def probandoTemplate(request):
     if "entrada" in request.GET:
         entrada = request.GET["entrada"]
-        gcode = ttg(entrada,1,0,"return",1).toGcode("M02 S500","M05 S0","G0","G1")
-        ser = serial.Serial('/dev/ttyUSB0')
-        print(ser.name)
-        ser.write(b'hello')
-        ser.close() 
-        return render(request, 'index.html', {"codigo": gcode})     
+        gcode = 'G1 X50 Y80'#ttg(entrada,1,0,"return",1).toGcode("M02 S500","M05 S0","G0","G1")
+        #SERIAL
+        index = 0
+        try:
+            ser = serial.Serial('COM3', 115200)
+            #while index < 3:
+            ser.write(gcode.encode())
+            respuesta_bytes = ser.read_until(b'\r\n')
+            respuesta_string = respuesta_bytes.decode('utf-8')
+            print("Codigo - " + str(index) + " : " + respuesta_string)
+            ser.close()
+        except Exception as e:
+            print(f"Error: {e}")
+        return render(request, 'index.html', {"codigo": gcode, "respuesta2": "exito"})
     else:
         respuesta = "no datos"
     return render(request, 'index.html', {"respuesta": respuesta})
